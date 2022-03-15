c4b121152ffd7558c3136eff3d9c5cce58b8dc7e
Austin Cappuccio <cappuca@email.sc.edu>
2022-03-15 17:41:50 -0400

    First Sprint!

---
+++ DataLoader.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class DataLoader {
+    
+    public DataLoader() {
+        
+    }
+
+    public ArrayList<FlightTicket> getFlights() {
+        return null;
+    }
+
+    public ArrayList<Hotel> getHotel() {
+        return null;
+    }
+
+
+}
---
+++ DataWriter.java
@@ -0,0 +1,19 @@
+public class DataWriter {
+    
+    public DataWriter() {
+
+    }
+
+    public void newUser(User user) {
+
+    }
+
+    public void newFlight(FlightTicket flight) {
+
+    }
+
+    public void newHotel(Hotel hotel) {
+
+    }
+
+}
