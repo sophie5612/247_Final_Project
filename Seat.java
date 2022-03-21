aa9b6417f051050d67949e3da2250b47558b37ae
Ben Goodman <nighttroop188@live.com>
2022-03-20 22:45:57 -0400

    Changes made by Ben

--- Seat.java
+++ Seat.java
@@ -5,21 +5,25 @@
 
 //import java.util.ArrayList;
 
-public class Seat { // This class might be useless
+public class Seat { 
+    private int rows = 10; //Change when dimensions of plane are given
+    private int col = 10;
     private int[][] location;
     private boolean isAvailable;
     private int x = 0;
     private int y = 0;
 
-   // public boolean Seat() {
-      //  for(int i = 0; i < rows; i++) {
-        //    for(int j = 0; j < columns; j++) { //Third time checking if seat is availible is repeated.
-
-        //    }
-        //}
-    //}
-    public boolean isSeatAvailable(String[][] seats) { //edit method
-        for(int i = 0; i < row; i++) {
+    public Seat() {
+        this.rows = rows;
+        this.col = col
+        this.location = location;
+        this.isAvailable = isAvailable;
+        this.x = x;
+        this.y = y;
+    }
+     
+    public boolean isSeatAvailable(String[][] seats) { 
+        for(int i = 0; i < rows; i++) {
             for(int j = 0; j < col; j++) {
                 if(seats[i][j] == "O"){
                     return true;
