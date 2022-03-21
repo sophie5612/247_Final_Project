493ef9e294f3e79c2624a01231920c1429113d59
Ben Goodman <nighttroop188@live.com>
2022-03-20 23:01:15 -0400

    Changes made to seat by Ben

--- Seat.java
+++ Seat.java
@@ -8,30 +8,23 @@
 public class Seat { 
     private int rows = 10; //Change when dimensions of plane are given
     private int col = 10;
-    private int[][] location;
-    private boolean isAvailable;
+    private String[][] location;
     private int x = 0;
     private int y = 0;
 
     public Seat() {
-        this.rows = rows;
-        this.col = col;
-        this.location = location;
-        this.isAvailable = isAvailable;
         this.x = x;
         this.y = y;
     }
      
-    public boolean isSeatAvailable(String[][] seats) { 
+    public boolean isSeatAvailable(String[][] location) { 
         for(int i = 0; i < rows; i++) 
         {
             for(int j = 0; j < col; j++) 
             {
-                if(seats[x][y] == "O"){
+                if(location[x][y] == "O")
+                {
                     return true;
-                }
-                else {
-                    return false;
                 }
             }
         }
