9121b2fb0fe411442e2727ab4b182cd816449ab7
Reagan <reagan@DESKTOP-5KICO17.localdomain>
2022-03-22 21:58:42 -0400

    hotel stuff

--- Hotel.java
+++ Hotel.java
@@ -3,7 +3,8 @@ import java.util.*;
  * @author Ben Goodman
  */
 public class Hotel extends Booking {
-    
+    protected int row;
+    protected int col;
     private UUID ID;
     private Date checkInDate;
     private Date checkOutDate;
@@ -11,13 +12,58 @@ public class Hotel extends Booking {
     private boolean noSmoking;
     private int numOfBeds;
     private boolean hasPool;
-    private ArrayList<Room> rooms;
+    private String[][] rooms = new String[row][col];
 
-    public Hotel() {
-
+    public Hotel(){
+        this.ID = UUID.randomUUID();
+        this.nameOfHotel = " ";
+        this.checkInDate = new Date();
+        this.checkOutDate = new Date();
+        this.noSmoking = false;
+        this.numOfBeds = 0;
+        this.hasPool = false;
     }
 
-    private int[][] showRooms() {
-        return new int[0][0];
+    public Hotel(UUID ID, String namOfHotel, Date checkInDate, Date checkOutDate, boolean noSmoking, String[][] rooms, boolean hasPool){
+            this.ID = ID;
+            this.nameOfHotel = namOfHotel;
+            this.checkInDate = checkInDate;
+            this.checkOutDate = checkOutDate;
+            this.noSmoking = noSmoking;
+            this.rooms = rooms;
+            this.hasPool = hasPool;
+        }
+
+    /**
+     *  A method that will sort flights based on default sorting behavior
+     * @param destinationCity
+     * @param departDate
+     */
+    
+    /**
+     * Calculate the number of unbooked seats
+     * @return The number of remaining seats 
+     */
+    public UUID getUuid() {
+        return ID;
+    }
+    public String getHotel() {
+        return nameOfHotel;
+    }
+    public Date getCheckinDate() {
+        return checkInDate;
+    }
+    public Date getCheckoutDate() {
+        return checkOutDate;
+    }
+    public boolean getSmoking() {
+        return noSmoking;
+    }
+    public String[][] getRooms() {
+        return rooms;
+    }
+    public boolean getPool() {
+        return hasPool;
     }
 }
+
