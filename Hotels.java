03fa1142eb3faf6d6a179a24780d370d8df0c450
Reagan <reagan@DESKTOP-5KICO17.localdomain>
2022-03-22 22:11:31 -0400

    hotel stuff

--- Hotels.java
+++ Hotels.java
@@ -1,6 +1,31 @@
-/**
- * @author 
- */
+import java.util.*;
+
 public class Hotels {
-    // replicate Flights
+
+    private static Hotels hotels = null;
+    private static ArrayList<Hotel> hotelList;
+
+    private Hotels() {
+        hotelList = DataLoader.loadHotels();
+    }
+
+    public static Hotels getInstance() {
+        if (hotels == null) {
+            hotels = new Hotels();
+        }
+
+        return hotels;
+    }
+
+    public ArrayList<Hotel> getHotels() {
+        return hotelList;
+    }
+
+    public void Hotel(UUID ID, String namOfHotel, Date checkInDate, Date checkOutDate, boolean noSmoking, String[][] rooms, boolean hasPool) {
+        hotelList.add(new Hotel(ID, namOfHotel, checkInDate, checkOutDate, noSmoking, rooms, hasPool));
+    }
+
+    public void logout() {
+        DataWriter.saveHotel();
+    }
 }
