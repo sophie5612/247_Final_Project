dc609d5025deab5f2e7133dcffdfb565857a5da1
Reagan <reagan@DESKTOP-5KICO17.localdomain>
2022-04-10 18:43:39 -0400

    Compare to Test

--- HotelsTest.java
+++ HotelsTest.java
@@ -10,13 +10,13 @@ public class HotelsTest {
     Hotel pricyHotel = new Hotel(UUID.randomUUID(), "EvesINN", true, 300.00, 4.5, null, "Charleston");
 
     @Test 
-    public void testCheapestFlight(){
+    public void testCheapestHotel(){
         int comp = cheapHotel.compareTo(pricyHotel); 
         assertEquals(-1, comp);
     }
 
     @Test
-    public void testPriciestFlight(){
+    public void testPriciestHotel(){
         int comp = pricyHotel.compareTo(cheapHotel);
         assertEquals(1, comp);
 
