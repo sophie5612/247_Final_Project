6018bcaefa8939b806ac8bff92801f25fe598fb9
Reagan <reagan@DESKTOP-5KICO17.localdomain>
2022-04-10 18:42:27 -0400

    CompareTo Hotels test

--- HotelsTest.java
+++ HotelsTest.java
@@ -1,10 +1,31 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.UUID;
 import org.junit.Test;
 
+
 public class HotelsTest {
-    //@Test
-    //public void testGetInstance()
-    //{
-    //    Hotels hotel = Hotels.getInstance(null);
-    //    assertEquals();
-    //}
-}
+    Hotel cheapHotel = new Hotel(UUID.randomUUID(), "EvesINN", true, 120.00, 2.3, null, "Charleston");
+    Hotel anotherCheapHotel = new Hotel(UUID.randomUUID(), "EvesINN", true, 120.00, 3.0, null, "Charleston");
+    Hotel pricyHotel = new Hotel(UUID.randomUUID(), "EvesINN", true, 300.00, 4.5, null, "Charleston");
+
+    @Test 
+    public void testCheapestFlight(){
+        int comp = cheapHotel.compareTo(pricyHotel); 
+        assertEquals(-1, comp);
+    }
+
+    @Test
+    public void testPriciestFlight(){
+        int comp = pricyHotel.compareTo(cheapHotel);
+        assertEquals(1, comp);
+
+    }
+
+    @Test
+    public void testEqualPrice(){
+        int comp = cheapHotel.compareTo(anotherCheapHotel);
+        assertEquals(0, comp);
+    }
+ }
+    
